e44df34a50203963dd213ea697f4213a883264f6
Archcoin <archcoin@gmx.us>
2014-10-04 20:39:38 -0400

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Archcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ArchcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
